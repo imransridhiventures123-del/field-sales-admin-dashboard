d34421a475ce815dd7d31f24d2ef3d863fdf11ac
sridhi-ventures <imran.sridhiventures123@gmail.com>
2026-08-17 17:04:09 +0530

    update env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,9 +1,9 @@
 // FILE: src/api/axiosInstance.js
 // OWNER: Imran
 import axios from "axios";
-// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-const BASE_URL = "http://localhost:5000";
+// const BASE_URL = "http://localhost:5000";
 
 const axiosInstance = axios.create({ baseURL: BASE_URL, headers: { "Content-Type": "application/json" } });
 axiosInstance.interceptors.request.use((config) => {
